c44a39da932c7d21cdf4a7bec750c92b76879dc3
filipkk78 <filipkk78@gmail.com>
2024-07-12 16:44:19 +0200

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,7 +1,36 @@
 function add(a, b) {
-    return a+b;
+    return a + b;
 }
 
-function substract() {
+function subtract(a, b) {
+    return a - b;
+}
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstOperand;
+let secondOperand;
+let operator;
+
+function operate(num1, num2, operation) {
+    switch(operation) {
+        case adding:
+            return add(num1, num2);
+            break;
+        case subtraction:
+            return subtract(num1, num2);
+            break;
+        case multiplication:
+            return multiply(num1, num2);
+            break;
+        case division:
+            return divide(num1, num2);
+            break;
+    }
 }
